19b5cd48222e49ae150aa224d69a16897fed3165
Trần Trọng Nhân <nhatduy1284t@gmail.com>
2021-06-20 11:18:04 +0700

    'tao_nhanh_devB_code_linh_tinh'

--- controllers/devB.js
+++ controllers/devB.js
@@ -3,3 +3,7 @@ console.log('Dev b code chức năng này nha !');
 function devB() {
     console.log('ghi gì cũng được ! ! ! !');
 }
+
+function createBranchDevB(){ 
+    console.log('vừa tạo nhánh dev B');
+}
